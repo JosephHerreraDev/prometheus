79266db4bf6058353b63c5fa9efadd2da9e4c5e5
Joseph Herrera <josephherreraleon@gmail.com>
2026-04-06 17:23:53 -0600

    add limine in login all script

--- install/login/all.sh
+++ install/login/all.sh
@@ -1,4 +1,4 @@
 bash $PROMETHEUS_INSTALL/login/plymouth.sh
 # bash $PROMETHEUS_INSTALL/login/default-keyring.sh
 bash $PROMETHEUS_INSTALL/login/sddm.sh
-# bash $PROMETHEUS_INSTALL/login/limine-snapper.sh
+bash $PROMETHEUS_INSTALL/login/limine-snapper.sh
