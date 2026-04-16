645a9e4a262ffe1e17629036111ee8418a36f8c0
Joseph Herrera <josephherreraleon@gmail.com>
2026-04-16 17:02:06 -0600

    remove plymouth and limine-snapper

--- install/login/plymouth.sh
+++
@@ -1,4 +0,0 @@
-if [[ $(plymouth-set-default-theme) != "prometheus" ]]; then
-  sudo cp -r "$HOME/.local/share/prometheus/default/plymouth" /usr/share/plymouth/themes/prometheus/
-  sudo plymouth-set-default-theme prometheus
-fi
